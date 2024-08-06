2df6ece91f29f2c2f3e9c0634f662ea5020e16c8
dashakotets <dashacot1@gmail.com>
2024-08-06 17:52:12 +0100

    task4

--- js/task-4.js
+++ js/task-4.js
@@ -3,18 +3,20 @@ const myForm = document.querySelector('form.login-form');
 const onFormSubmit = event => {
     event.preventDefault();
 
+    const formData = {
+    email: myForm.elements.email.value,
+    password: myForm.elements.password.value,
+    }
+
     if (!myForm.elements.email.value || !myForm.elements.password.value) {
         alert('All form fields must be filled in');
     } else {
-        const formData = {
-        email: myForm.elements.email.value,
-        password: myForm.elements.password.value,
+        console.log(formData);
+        myForm.reset();
     };
-    }
+    
 
-    console.log(formData);
-
-    myForm.reset();
+    
 };
 
 myForm.addEventListener('submit', onFormSubmit);
